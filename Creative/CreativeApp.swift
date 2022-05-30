857bdfdbc5fec3195fcb781069a0bb899111977f
Konstantin Fomenkov <kolij2005@gmail.com>
2022-05-30 19:28:45 +0300

    Initial Commit

---
+++ Creative/CreativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreativeApp.swift
+//  Creative
+//
+//  Created by Konstantin Fomenkov on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CreativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
